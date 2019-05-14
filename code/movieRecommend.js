c2be030e558dd75533380dce81f646f3c8b4fced
hoik92 <hoik92@nate.com>
2019-05-14 13:21:12 +0900

    영화를 추천하는 캡슐

---
+++ code/movieRecommend.js
@@ -0,0 +1,85 @@
+const genres = require('./lib/genres.js')
+var http = require('http')
+var console = require('console')
+
+module.exports.function = function movieRecommend (inputGenre) {
+  console.log(inputGenre)
+  const baseImageUrl = 'https://image.tmdb.org/t/p/w500/'
+  const baseUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=295b8efeee781caec375a1c950261736&language=ko-KR&region=KR&page='
+  const url = 'https://api.themoviedb.org/3/movie/popular?api_key=295b8efeee781caec375a1c950261736&language=ko-KR&page=1&region=KR'
+  
+  const results = []
+  let inGenre = false
+  
+  if (inputGenre) {
+    if (inputGenre.slice(inputGenre.length - 2, inputGenre.length) === '영화'){
+      inputGenre = inputGenre.slice(0, inputGenre.length - 2)
+    }
+    const inputGenreId = getKeyByValue(genres, inputGenre) * 1
+    let page = 1
+    while (results.length < 20 && page < 10) {
+      var response = http.getUrl(baseUrl + page.toString(), {format: 'json'})
+      response.results.forEach(function(result){
+        result.genre_ids.forEach(function(genre) {
+          if (genre === inputGenreId) {
+            inGenre = true
+          }
+        })
+        if (inGenre) {
+          const data = {
+              movieTitle: result.title,
+              movieGenre: MakeGenre(result.genre_ids),
+              // movieGenre: result.genre_ids.join(', '),
+              movieScore: result.vote_average,
+              moviePosterUrl: baseImageUrl + result.poster_path,
+              movieOpenDate: result.release_date,
+              movieDescription: result.overview + ' '
+          }
+          if(data.movieDescription === ' ') {
+            data.movieDescription = '해당 영화에 대한 요약 정보가 없습니다.'
+          }
+          results.push(data)
+          inGenre = false
+        }
+      })
+      page++
+    }
+  } else {
+    var response = http.getUrl(url, {format: 'json'})
+    response.results.forEach(function(result){
+      const data = {
+        movieTitle: result.title,
+        movieGenre: MakeGenre(result.genre_ids),
+        // movieGenre: result.genre_ids.join(', '),
+        movieScore: result.vote_average,
+        moviePosterUrl: baseImageUrl + result.poster_path,
+        movieOpenDate: result.release_date,
+        movieDescription: result.overview + ' '
+      }
+      if(data.movieDescription === ' ') {
+        data.movieDescription = '해당 영화에 대한 요약 정보가 없습니다.'
+      }
+      results.push(data)
+    })
+  }
+
+  return results
+}
+
+function MakeGenre(array) {
+  const list = [];
+  array.forEach(function(genre_id) {
+    list.push(genres[genre_id])
+  })
+  return list.join(', ')
+}
+
+function getKeyByValue(object, value) {
+  let result
+  for (let key in object) {
+    if (object[key] == value) {
+      result = key
+    }
+  }
+  return result
+}
---
+++ code/movieUpComming.js
@@ -0,0 +1,37 @@
+const genres = require('./lib/genres.js')
+module.exports.function = function movieUpComming () {
+  const http = require('http')
+  const console = require('console')
+  
+  const baseImageUrl = 'https://image.tmdb.org/t/p/w500/'
+  const url = 'https://api.themoviedb.org/3/movie/upcoming?api_key=295b8efeee781caec375a1c950261736&language=ko-KR&page=1&region=KR'
+  const response = http.getUrl(url, {format: 'json'})
+
+  const results = []
+  
+  response.results.forEach(function(result){
+    const data = {
+      movieTitle: result.title,
+      movieGenre: MakeGenre(result.genre_ids),
+      // movieGenre: result.genre_ids.join(', '),
+      movieScore: result.vote_average,
+      moviePosterUrl: baseImageUrl + result.poster_path,
+      movieOpenDate: result.release_date,
+      movieDescription: result.overview + ' '
+    }
+    if(data.movieDescription === ' ') {
+      data.movieDescription = '해당 영화에 대한 요약 정보가 없습니다.'
+    }
+    results.push(data)
+  })
+  
+  return results
+}
+
+function MakeGenre(array) {
+  const list = [];
+  array.forEach(function(genre_id) {
+    list.push(genres[genre_id])
+  })
+  return list.join(', ')
+}
